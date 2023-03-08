0a69144f7938be682383cd2ea6bd5b7f60d53020
g3th3x <g3th3x@gmail.com>
2023-03-08 04:39:22 +0300

    Task 2 done

--- task2/index.php
+++ task2/index.php
@@ -39,7 +39,19 @@
 
 # Можно использовать список:
 
-$list = array (
+// $list = array(
+//     "10:00-14:00",
+//     "16:00-20:00"
+// );
+
+// $trying = array(
+// "09:00-11:00",
+// "11:00-13:00",
+// "14:00-16:00",
+// "14:00-17:00",
+// );
+
+$list = array(
 	'09:00-11:00',
 	'11:00-13:00',
 	'15:00-16:00',
@@ -47,3 +59,71 @@ $list = array (
 	'20:30-21:30',
 	'21:30-22:30',
 );
+
+$trying = array(
+    "09:00-11:00",
+    "11:00-13:00",
+    "14:00-16:00",
+    "14:00-17:00",
+    "00:00-14:00",
+    "16:00-20:00",
+    "24:00-21:00",
+    "23:61-21:61",
+    "23:00-21:61",
+    "23:61-21:00",
+    "01:00-24:00",
+    "20:30-17:30",
+    "08:00-08:30",
+    "09:09-08:08",
+    "23:00-08:15",
+);
+
+function is_valid_time($time){
+    return (preg_match_all("#^(?:2[0-3]|[01][0-9]):[0-5][0-9]-(?:2[0-3]|[01][0-9]):[0-5][0-9]$#", $time, $match)) ? true : false;
+}
+
+function is_crossing($timeTry){
+    global $list;
+
+    preg_match_all("#^((?:2[0-3]|[01][0-9]):[0-5][0-9])-((?:2[0-3]|[01][0-9]):[0-5][0-9])$#", $timeTry, $match);
+    $time2_start = $match[1][0];
+    $time2_end = $match[2][0];
+
+    if($time2_start > $time2_end) {
+        $overTime[] = $time2_start."-23:59";
+        $overTime[] = "00:00-".$time2_end;
+        foreach($overTime as $time) {
+            if(is_crossing($time)) {
+                return true;
+            }
+        }
+            return false;
+    }
+
+    foreach($list as $time) {
+        preg_match_all("#^((?:2[0-3]|[01][0-9]):[0-5][0-9])-((?:2[0-3]|[01][0-9]):[0-5][0-9])$#", $time, $match);
+        $time1_start = $match[1][0];
+        $time1_end = $match[2][0];
+            
+        if(strtotime($time1_start) < strtotime($time2_end) and strtotime($time1_end) > strtotime($time2_start)){
+            return true;
+        }
+    }
+        return false;
+}
+
+function interval_crossing($timeTry){
+    if(is_valid_time($timeTry)) {
+        if(is_crossing($timeTry)) {
+            echo "{$timeTry} => произошло наложение<br>";
+        } else {
+            echo "{$timeTry} => наложения нет<br>";
+        }
+    } else {
+        echo "ERR: {$timeTry} - ошибка ввода параметра<br>";
+    }
+}
+
+foreach($trying as $time) {
+    interval_crossing($time);
+}
